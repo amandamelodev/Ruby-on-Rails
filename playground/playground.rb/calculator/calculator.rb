e7737a236411fc81b0d7732462e8aaf8d08b3c6a
Amanda Maciel de Melo <amanda.maciel@ulbra.inf.br>
2022-01-11 13:58:04 -0300

    Create calculator.rb

---
+++ playground/playground.rb/calculator/calculator.rb
@@ -0,0 +1,29 @@
+puts "simple calculator"
+25.times {print "--"}
+puts "Enter the first number: "
+num_1 = gets.chomp
+puts "Enter the second number: "
+num_2 = gets.chomp. 
+puts "Choose one operation \n 1- Addition \n 2- Subtraction \n 3- Multiplication \n 4- Division \n 5- Modulus"
+
+input gets.chomp
+
+case input
+when "1"
+    puts "The first number added for by the second number is #{num_1.t_float + num_2.t_float}"
+when "2"
+    puts "The first number subtracted for by the second number is #{num_1.t_float + num_2.t_float}"
+when "3"
+    puts "The first number multiplied for by the second number is #{num_1.t_float + num_2.t_float}"
+when "4"
+    puts "The first number multiplied for by the second number is #{num_1.t_float + num_2.t_float}"
+when "5"
+    puts " The first number mod the second number is #{num_1.to_float % num_2.to_float"
+    #% ou mod sobra da divisao
+else 
+    puts " You typed #{input} - type either 1, 2, 3, 4 or 5 to get the correct answer."
+end    
+
+35.times {print "-"}
+puts "\n Thanks for using calculator.\n Refrsh to restart."
+    
